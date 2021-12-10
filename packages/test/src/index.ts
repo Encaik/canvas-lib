d2fc11e13d4eb18164aa698e3e4258b6fa7937ce
shatong <843391622@qq.com>
2021-12-10 14:55:46 +0800

    feat:init

---
+++ packages/test/src/index.ts
@@ -0,0 +1,3 @@
+import {Color} from '@canvas-lib/core';
+
+console.log(new Color("#fff"))
---
+++ packages/test/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+	entry: "./src/index.ts",
+	mode: "development",
+	devtool: "inline-source-map",
+	devServer: {
+		static: "./dist",
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: "canvas-lib调试页面",
+		}),
+	],
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: "ts-loader",
+				exclude: /node_modules/,
+			},
+		],
+	},
+	resolve: {
+		extensions: [".tsx", ".ts", ".js"],
+	},
+	output: {
+		filename: "main.js",
+		path: path.resolve(__dirname, "dist"),
+		clean: true,
+	},
+};
